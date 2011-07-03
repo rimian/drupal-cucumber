364abd48669453d3b4a4af800a2eb8baac22f907
Rimian Perkins <hello@rimian.com.au>
2011-07-03 23:33:48 +1000

    login test passes

--- features/hello.feature
+++ features/hello.feature
@@ -7,3 +7,8 @@ Feature: Hello
     Given I bootstrap Drupal
     When I visit the home page
     Then I should see the website title
+    
+  Scenario: Log in
+    Given I am on the login page
+    When I submit the login form
+    Then I should be logged in
--- features/step_definitions/hello.rb
+++ features/step_definitions/hello.rb
@@ -1,9 +1,24 @@
 
+Given /^I am on the login page$/ do
+  visit "http://localhost/user"
+end
 
 When /^I visit the home page$/ do
   visit "http://localhost"
 end
 
+When /^I submit the login form$/ do
+  fill_in "name", :with => "admin"
+  fill_in "pass", :with => "cucumber"
+  click_button
+end
+
 Then /^I should see the website title$/ do
   response_body.should contain("User login")
 end
+
+Then /^I should be logged in$/ do
+  reload
+  response_body.should contain("Log out")
+end
+
